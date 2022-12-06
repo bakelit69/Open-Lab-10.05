61daa18df2f5b79ffa443e9252476b68407a3970
z21ista13 <z21ista13@z00p1-08.SPSKNM>
2022-12-06 08:14:03 +0100

    Add project files.

---
+++ Open-Lab-10.05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_10._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
