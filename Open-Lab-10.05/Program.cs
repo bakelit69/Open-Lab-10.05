4a4a5767a7aa7800adcf538427eb8ad46cfa1df2
bakelit69 <jakubchalupka385@gmail.com>
2022-12-08 21:08:35 +0100

    finalised IT ;)

--- Open-Lab-10.05/Program.cs
+++ Open-Lab-10.05/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Open_Lab_10._05;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,27 @@ using System.Threading.Tasks;
 
 namespace Open_Lab_10._05
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            Book LOTR = new Book();
+            /*LOTR.Title = "Lord Of The Rings";
+            LOTR.Pages = 576;
+            LOTR.Category = "Fantasy";
+            LOTR.Author = "J.R.R.Tolkien";
+            LOTR.ReleaseDate = 1954;*/
+            Console.WriteLine(LOTR.ToString());
+
+            Book LOTR2 = new Book();
+            Console.WriteLine(LOTR2.ToString());
+
+            Book LOTR3 = new Book("LOTR3", 576);
+            Console.WriteLine(LOTR3.ToString());
+
+            Book HOBIT = new Book("HOBIT", 492, Book.categoryList[3], "J.R.R.Tolkien", 1954);
+
+            Console.WriteLine(HOBIT.ToString());
         }
     }
 }
